d47d316868c66b34a597988ca45821367904c48c
Aditya <avshetty98@gmail.com>
2022-06-19 19:13:11 +0530

    initial commit

---
+++ src/Component/Map.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+function MapCpmponent(props) {
+    console.log('props', props);
+    console.log('props.google', props.google);
+    return (
+        <Map
+            google={props.google}
+            style={{ width: '50vw', height: '50vh' }}
+            initialCenter={{
+                lat: 22.258652,
+                lng: 71.192383
+            }}
+        >
+
+        </Map>
+    )
+};
+
+export default GoogleApiWrapper({
+    apiKey: 'Enter your Api kew from console.cloud.google',
+})(MapCpmponent);
